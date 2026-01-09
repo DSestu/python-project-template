7a101509383cb3392862e037e7ddd7464fdd22dd
DSestu <david.sestu@gmail.com>
2026-01-09 19:46:41 +0100

    chore: update dependencies in pyproject.toml and implement Entrypoint class in main.py for command handling

--- {{cookiecutter.project_name}}/app/main.py
+++ {{cookiecutter.project_name}}/app/main.py
@@ -1,5 +1,27 @@
+import webbrowser
+
+from fire import Fire
+
 from app.src.my_import import hello_world_string
 
 
+class Entrypoint:
+    """This docstring is the main documentation"""
+
+    def __call__(self, url: str | None = None) -> None:
+        """Default command"""
+        self.main(url)
+
+    def main(self, url: str | None = None) -> None:
+        """Main command"""
+        print(hello_world_string)
+        if url:
+            webbrowser.open(url)
+
+    def other_command(self) -> None:
+        """Another command example"""
+        print("This is another command")
+
+
 def main() -> None:
-    print(hello_world_string)
+    Fire(Entrypoint)
